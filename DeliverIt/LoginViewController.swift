ebeec15718751a9f2925a9f964d0277ae1fce48f
Lovpreet Hansra <lhansra@ucsd.edu>
2020-04-16 19:11:46 -0700

    Created Parse server and connected client.

---
+++ DeliverIt/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  DeliverIt
+//
+//  Created by Lovpreet Hansra on 4/16/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func login(_ sender: Any) {
+    }
+    
+    
+    @IBAction func signUp(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
